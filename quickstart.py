35f917023bdf8859311f97486c07dd7422fdf6e5
DMYTRO BORIICHUK <dmytro.boriichuk@dxc.com>
2023-02-21 16:48:00 +0200

    cleaned up comments

--- quickstart.py
+++ quickstart.py
@@ -29,22 +29,6 @@ def main():
     service_account_info = json.load(open('service-credentials.json'))
     creds = Credentials.from_service_account_info(service_account_info)
 
-    '''
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    '''
-
     try:
         service = build('sheets', 'v4', credentials=creds)
 
